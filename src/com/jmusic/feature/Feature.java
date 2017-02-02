2f70a9f79f715813a3afeece2068afd457091130
deepankar <deepankar11093@gmail.com>
2017-02-03 00:57:37 +0530

    adds abstract feature class and feature spectral_centroid

---
+++ src/com/jmusic/feature/Feature.java
@@ -0,0 +1,21 @@
+package com.jmusic.feature;
+
+/**
+ * Abstract class for a feature
+ * 
+ * @author Deepankar Agrawal
+ *
+ */
+public abstract class Feature<T extends Number> {
+	
+	protected double[] signal;
+	protected double   samplingRate;
+	
+	public void setValues(double[] signal, double samplingRate){
+		this.signal = signal;
+		this.samplingRate = samplingRate;
+	}
+	
+	public abstract T evaluate();
+
+}
